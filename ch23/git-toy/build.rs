9e9708db20836962d2fb7cd151e5d8253a9e3b8a
Your Name <you@example.com>
2024-11-25 01:41:19 +0900

    오타 수정

--- ch23/git-toy/build.rs
+++ ch23/git-toy/build.rs
@@ -2,8 +2,6 @@ use cmake::Config;
 
 fn main() {
     let dst = Config::new("libgit2").build();
-    println!("cargo:rustc-link-search=native={}/build", dst.display());
+    println!("cargo::rustc-link-search=native={}/build", dst.display());
     println!("cargo::rustc-link-lib=git2");
-
-    println!("cargo")
 }
